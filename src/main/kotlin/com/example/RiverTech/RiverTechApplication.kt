caf1aeb455fd8ae407f1f9c1030c306953a03e37
ReubenXuereb <56310716+ReubenXuereb@users.noreply.github.com>
2024-11-09 15:48:00 +0100

    First commit

---
+++ src/main/kotlin/com/example/RiverTech/RiverTechApplication.kt
@@ -0,0 +1,11 @@
+package com.example.RiverTech
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RiverTechApplication
+
+fun main(args: Array<String>) {
+	runApplication<RiverTechApplication>(*args)
+}
